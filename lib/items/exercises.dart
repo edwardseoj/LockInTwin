af3aaa8e037fff58e91f4aa7fe74e33a6f81fc70
Edward Seoj <202301386@iacademy.edu.ph>
2025-11-24 17:28:37 +0800

    Created Exercise items class

---
+++ lib/items/exercises.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Exercises{
+  String title;
+  String subtitle;
+  Widget icon;
+  bool isSelected;
+
+  Exercises({
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.isSelected,
+  });
+}
