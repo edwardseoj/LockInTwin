9e0efd4fb41db4a7d2a54a96ada09eff515fdf0e
Edward Seoj <ciabattacereals@gmail.com>
2025-11-29 01:39:43 +0800

    Working slidable, spaghetti

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:lock_in_twin/create_routine.dart';
 
 void main() {
@@ -69,29 +70,68 @@ class MyApp extends StatelessWidget {
               children: [
                 if (routines.isEmpty) _emptyRoutinesWidget(),
 
-                //put a list view here if routines is not empty
-                ListView.separated(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: routines.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Center(
-                      child: Container(
-                        padding: Padding(padding: EdgeInsets.all(10)).padding,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Text(
-                          routines[index],
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return SizedBox(height: 10);
-                  },
+                SlidableAutoCloseBehavior(
+                  child: Center(
+                    child: ListView.separated(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: routines.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Align(
+                          alignment: Alignment.center,
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(maxWidth: 300),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                              child: Slidable(
+                                groupTag: '0',
+                                endActionPane: ActionPane(
+                                  extentRatio: 0.25,
+                                  motion: const BehindMotion(),
+                                  children: [
+                                    SlidableAction(
+                                      onPressed: (context) {
+                                        if (kDebugMode) {
+                                          print("Delete ${routines[index]}");
+                                        }
+                                      },
+                                      backgroundColor: Colors.red,
+
+                                      icon: Icons.delete_outline,
+                                    ),
+                                  ],
+                                ),
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(20),
+                                      bottomLeft: Radius.circular(20),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    routines[index],
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(height: 10);
+                      },
+                    ),
+                  ),
                 ),
               ],
             ),
