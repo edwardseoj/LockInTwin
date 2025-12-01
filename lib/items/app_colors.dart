2d5d432d2f11965f3fe2414e9d1a0b3fb708769d
Edward Seoj <ciabattacereals@gmail.com>
2025-12-01 16:32:03 +0800

    Paolo's app color class

---
+++ lib/items/app_colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Gradient colors
+  static const Color gradientStart = Color(0xFF048BA8);
+  static const Color gradientEnd   = Color(0xFF2F2D2E);
+
+  // Button color
+  static const Color buttonBlue    = Color(0xFF2E4057);
+
+  // Container background
+  static const Color containerWhite = Colors.white;
+
+  // Screen background
+  static const Color screenGrey = Color(0xFFE0E0E0);
+}
